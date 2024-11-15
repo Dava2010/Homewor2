a5a90072d5233ec5cac74b6622cc88678e722e94
Dava2010 <davlatbek20101106@gmail.com>
2024-11-15 18:07:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = 5;
+let b = 853290;
+const sux = 87;
+
+let userName = prompt("Введите имя");
+let surName = prompt("Введите фамилию");
+let ageName = prompt("Введите ваш возраст");
+let favoritesubject = prompt("Введите ваш любимый предмет");
+let livingdistrict = prompt("Введите ваш Район");
+alert(`Благодарим за информацию! ${userName}, ${surName}, ${ageName}, ${favoritesubject}, ${livingdistrict}`);
